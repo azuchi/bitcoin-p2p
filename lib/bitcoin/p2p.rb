41350879bdedeaa1ac694e1320b4fd4be55fd7a9
azuchi <azuchi@chaintope.com>
2021-01-19 12:00:27 +0900

    initial commit

---
+++ lib/bitcoin/p2p.rb
@@ -0,0 +1,8 @@
+require "bitcoin/p2p/version"
+
+module Bitcoin
+  module P2P
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/bitcoin/p2p/version.rb
@@ -0,0 +1,5 @@
+module Bitcoin
+  module P2P
+    VERSION = "0.1.0"
+  end
+end
